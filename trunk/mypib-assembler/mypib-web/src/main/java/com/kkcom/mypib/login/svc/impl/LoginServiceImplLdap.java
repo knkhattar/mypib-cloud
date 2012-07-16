4655eb41090a18c289338476a19b3c019fb446db
knkhattar@gmail.com <knkhattar@gmail.com@a3fc7eec-ed17-7e42-9ef4-956d25718bd7>
2012-07-16 14:05:14 +0000

    Updated for code

--- trunk/mypib-assembler/mypib-web/src/main/java/com/kkcom/mypib/login/svc/impl/LoginServiceImplLdap.java
+++ trunk/mypib-assembler/mypib-web/src/main/java/com/kkcom/mypib/login/svc/impl/LoginServiceImplLdap.java
@@ -11,12 +11,35 @@ public class LoginServiceImplLdap implements LoginService {
 
 	public boolean authenticate(String userid, String password) {
 		System.out.println(" Impl:: " + this);
-		boolean retVal = false;
-		if (userid.equals("testid") && password.equals("testpass")) {
-			retVal = true;
-		}
 
-		return retVal;
+		try {
+			Hashtable<String, String> env = new Hashtable<String, String>();
+			env.put(Context.INITIAL_CONTEXT_FACTORY,
+					"com.sun.jndi.ldap.LdapCtxFactory");
+
+			// replace with your server URL/IP
+			env.put(Context.PROVIDER_URL, "LDAP://XYZXYZ:389");
+
+			/*
+			 * only DIGEST-MD5 works with our Windows Active Directory
+			 */
+			env.put(Context.SECURITY_AUTHENTICATION, "DIGEST-MD5");
+
+			env.put(Context.SECURITY_PRINCIPAL, loginId);
+			env.put(Context.SECURITY_CREDENTIALS, passwd);
+			ctx = new InitialDirContext(env);
+			return true;
+		} catch (Exception ex) {
+			log.print("Authenticating user - " + loginId + " failed");
+		} finally {
+			if (ctx != null) {
+				try {
+					ctx.close();
+				} catch (Exception ex) {
+				}
+			}
+		}
+		return false;
 	}
 
 	public void init() {
