9597cd96907fa57a478f7602c1b28b0243456700
knkhattar@gmail.com <knkhattar@gmail.com@a3fc7eec-ed17-7e42-9ef4-956d25718bd7>
2012-07-29 13:00:17 +0000

    Updated to accept arguments in line

--- mypib-assembler/mypib-web/src/main/java/com/kkcom/mypib/MyPibUtils.java
+++ mypib-assembler/mypib-web/src/main/java/com/kkcom/mypib/MyPibUtils.java
@@ -12,29 +12,39 @@ import com.kkcom.mypib.constants.MyPibConstants;
 
 //import org.apache.commons.codec.binary.Base64;
 
+/*
+ * Compiled with jdk1.6.0
+ * */
 public class MyPibUtils {
-
+	
 	public static void main(String[] args) {
 		try {
+			// Program arguments 1. E or D, 2. string 3. seed
+			
 
-			//final String toBeEncrypted = "testid";//qM7Nw41vQw0=
-			final String toBeEncrypted = "testpass";//Nr6QJLD0Te+wa9+W55Z1zA==
-			String encryptedString = getEncryptedValue(toBeEncrypted);
-			System.out.println("encryptedString:: " + encryptedString);
-			String origilanString = getDecryptedValue(encryptedString);
-			System.out.println("origilanString:: " + origilanString);
+			final String eOrD = args[0];
+			final String toBeOperated = args[1];
+			final String seed = args[2];
+			if (eOrD.equalsIgnoreCase("E")) {
+				System.out.println("Encrypted value is "
+						+ getEncryptedValue(toBeOperated, seed));
+			} else {
+				System.out.println("Decrypted value is "
+						+ getDecryptedValue(toBeOperated, seed));
+
+			}
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 	}
 
-	public static  String getEncryptedValue(final String toBeEncrypted)
-			throws Exception {
+	public static String getEncryptedValue(final String toBeEncrypted,
+			final String seed) throws Exception {
 
-		
 		// INITIALISE THE VARIABLES
-		final String cryptoSeed = MyPibConstants.CRYPTO_SEED;
+		final String cryptoSeed = seed;
 		DESKeySpec keySpec = new DESKeySpec(cryptoSeed.getBytes("UTF8"));
 		SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
 		SecretKey key = keyFactory.generateSecret(keySpec);
@@ -45,15 +55,17 @@ public class MyPibUtils {
 		cipher.init(Cipher.ENCRYPT_MODE, key);
 		byte[] cleartext = toBeEncrypted.getBytes("UTF8");
 		byte[] newbyte = cipher.doFinal(cleartext);
-		String encrypedString = new String(Base64.encode(newbyte));
+		String encrypedString = new String(
+				org.springframework.security.crypto.codec.Base64
+						.encode(newbyte));
 		return encrypedString;
 	}
 
-	public static  String getDecryptedValue(final String toBeDecrpted)
-			throws Exception {
+	public static String getDecryptedValue(final String toBeDecrpted,
+			final String seed) throws Exception {
 
 		// INITIALISE THE VARIABLES
-		final String cryptoSeed = MyPibConstants.CRYPTO_SEED;
+		final String cryptoSeed = seed;
 		DESKeySpec keySpec = new DESKeySpec(cryptoSeed.getBytes("UTF8"));
 		SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
 		SecretKey key = keyFactory.generateSecret(keySpec);
@@ -72,3 +84,4 @@ public class MyPibUtils {
 	}
 
 }
+	
